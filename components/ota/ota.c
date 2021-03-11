48f6eea6eb622e551e966ff30c610281b51ad5ae
hokiuchow <hokiu.chow@gmail.com>
2021-03-11 13:18:15 +0800

    again

--- components/ota/ota.c
+++ components/ota/ota.c
@@ -45,7 +45,7 @@ void run_ota(void *params)
   while (true)
   {
     xSemaphoreTake(ota_semaphore, portMAX_DELAY);
-    ESP_LOGI(TAG, "Invoking OTA 3");
+    ESP_LOGI(TAG, "Invoking OTA");
 
     esp_http_client_config_t clientConfig = {
         .url = OTA_URL,
